9a78abd512bf5c7724e4dcacdb199b88a924fb00
Andreichev Michail <andreichev.m@mail.ru>
2022-02-16 14:14:08 +0300

    Controller Example update

--- SpringControllerExample/src/main/java/ru/itis/controllerexample/controller/DefaultController.java
+++ SpringControllerExample/src/main/java/ru/itis/controllerexample/controller/DefaultController.java
@@ -1,13 +1,19 @@
 package ru.itis.controllerexample.controller;
 
+import org.springframework.http.MediaType;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class DefaultController {
-    @GetMapping("/hello")
+
+    // Следующая запись идентична этой записи
+    // @GetMapping("/hello")
+    @RequestMapping(value = "/hello", method = RequestMethod.GET)
     @ResponseBody
     String defaultRequest() {
         System.out.println("IT WORKS");
@@ -19,5 +25,11 @@ public class DefaultController {
         model.addAttribute("message", "HELLO, WORLD!");
         System.out.println("Freemarker checking...");
         return "Greeting";
+    }
+
+    @RequestMapping(value = "/test", method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
+    @ResponseBody
+    String getStudent() {
+        return "{ \"name\": \"Ivan Ivanov\", \"university\": \"KPFU\" }";
     }
 }
--- SpringControllerExample/src/main/java/ru/itis/controllerexample/filter/DefaultFilter.java
+++ SpringControllerExample/src/main/java/ru/itis/controllerexample/filter/DefaultFilter.java
@@ -9,7 +9,7 @@ import java.io.IOException;
 public class DefaultFilter implements Filter {
 
     @Override
-    public void init(FilterConfig filterConfig) throws ServletException {}
+    public void init(FilterConfig filterConfig) {}
 
     @Override
     public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
