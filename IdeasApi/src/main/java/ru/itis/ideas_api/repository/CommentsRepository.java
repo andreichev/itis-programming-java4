ed5682201497a428da5b813aedb22c4cf1fab9e9
hasanova777 <hasssanova.177@gmail.com>
2022-04-29 11:23:46 +0300

    homework done

---
+++ IdeasApi/src/main/java/ru/itis/ideas_api/repository/CommentsRepository.java
@@ -0,0 +1,6 @@
+package ru.itis.ideas_api.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import ru.itis.ideas_api.model.Comment;
+
+public interface CommentsRepository extends JpaRepository<Comment, Long> { }
